4d5ed185328e28896c8808aa0fdc8eb5aca8580c
CristhianMera <leandromera2004@gmail.com>
2025-09-24 19:18:33 -0500

    Catalogo en el home

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,27 +1,23 @@
 import { Outlet, useLocation } from "react-router-dom";
 import { Navbar } from "../components/shared/Navbar";
 import { Footer } from "../components/shared/Footer";
+import { Banner } from "../components/home/Banner";
+import { Newsletter } from "../components/home/Newsletter";
 
 export const RootLayout = () => {
     const { pathname } = useLocation();
-
-	console.log(pathname);
 
 	return (
 		<div className='h-screen flex flex-col font-montserrat'>
 			<Navbar />
 
-			{pathname === '/' && (
-                <header className="my-8">Banner</header>
-            )}
+			{pathname === '/' &&  <Banner />}
 
-			<main className='container mx-auto px-4 my-8 flex-1'>
+			<main className='container my-8 flex-1'>
 				<Outlet />
 			</main>
 
-			{pathname === '/' && (
-                <header className="my-8">Newsletter</header>
-            )}
+			{pathname === '/' && <Newsletter />}
 			<Footer />
 		</div>
 	);
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,9 @@
+import { FeatureGrid } from "../components/home/FeatureGrid";
+
 export const HomePage = () => {
-    return <div>HomePage</div>;
+    return(
+        <div>
+            <FeatureGrid />
+        </div>
+    );   
 };
