8d5f3ead70e74405f5aaf4ba5180e005c7fca57f
Alonzo Mero Jose Alejandro <153472262+josealonzo2004@users.noreply.github.com>
2026-01-14 20:22:05 -0500

    cambio para deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 //aqui se exportan todas las paginas para facilitar las importaciones en otras partes de la aplicacion
 
-export { default as AboutPage } from './AboutPage';
-export { default as HomePage } from './HomePage';
-export { default as ProductsPage } from './ProductsPage';
-export { default as ProductDetail } from './ProductDetail';
-export { default as CartPage } from './CartPage';
-export { default as CheckoutPage } from './CheckoutPage';
-export { default as LoginPage } from './LoginPage';
-export { default as RegisterPage } from './RegisterPage';
-export { default as ProfilePage } from './ProfilePage';
-export { default as AdminDashboardPage } from './AdminDashboardPage';
+export { AboutPage } from './AboutPage';
+export { HomePage } from './HomePage';
+export { ProductsPage } from './ProductsPage';
+export { ProductDetail } from './ProductDetail';
+export { CartPage } from './CartPage';
+export { CheckoutPage } from './CheckoutPage';
+export { LoginPage } from './LoginPage';
+export { RegisterPage } from './RegisterPage';
+export { ProfilePage } from './ProfilePage';
+export { AdminDashboardPage } from './AdminDashboardPage';
